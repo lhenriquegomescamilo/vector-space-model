470005bed191db85065dee42d7889e5195445946
Luis camilo <lhenriquegomescamilo@gmail.com>
2020-03-09 00:18:22 -0300

    Starting the repository

---
+++ src/main/kotlin/com/camilo/main/VectorSpaceModelApplication.kt
@@ -0,0 +1,42 @@
+package com.camilo.main
+
+import com.sun.javaws.exceptions.InvalidArgumentException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.math.sqrt
+
+suspend fun main(args: Array<String>) = coroutineScope {
+    val y = arrayOf(2, 1, 3)
+    val z = arrayOf(1, 3, 2)
+
+    val cosineSimilarity = cosineSimilarity(y, z)
+    println("The value shoud be $cosineSimilarity")
+}
+
+suspend fun cosineSimilarity(y: Array<Int>, z: Array<Int>) = coroutineScope {
+    if (!y.size.equals(z.size)) throw InvalidArgumentException(arrayOf("The param Y and Z should be equal"))
+    val sumA = calculatingValue(y, z)
+    val sumB = async { calculoSqrt(y) }
+    val sumC = async { calculoSqrt(z) }
+    sumA.div(sumB.await() * sumC.await())
+
+}
+
+suspend fun calculatingValue(y: Array<Int>, z: Array<Int>) = coroutineScope {
+    var sum = 0;
+    for ((index, value) in y.withIndex()) {
+        val valueB = z[index]
+        sum += (value * valueB)
+    }
+    sum
+}
+
+suspend fun calculoSqrt(y: Array<Int>) = coroutineScope {
+
+    var sum = 0.0;
+    for (i in y) {
+        sum += Math.pow(i.toDouble(), 2.0)
+    }
+    sqrt(sum)
+}
+
---
+++ src/test/kotlin/com/camilo/main/VectorSpaceModelApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.camilo.main
+
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+
+//@SpringBootTest
+class VectorSpaceModelApplicationTests {
+
+//    @Test
+    fun contextLoads() {
+    }
+
+}
